b230f1e2524a16385cc0fa673d9e0279d2eebb94
karnse <suphawit.se@ku.th>
2023-02-09 16:18:23 +0700

    config database

---
+++ backend/config/database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
+username=os.getenv('name')
+password=os.getenv('password')
+client = MongoClient(f'mongodb://{username}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
+
+print(username,password)
+db = client['exceed03']
+menu = db['Deafualt_menu']
+order_count = db['Order_count']
+order_menu = db['Order_menu']
+Ingredient_status = db['Ingredient_status']
