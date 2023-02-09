a7388146bee7aa5f5941b49e7f4f302f62ff8579
karnse <suphawit.se@ku.th>
2023-02-09 17:12:36 +0700

    seasoning

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Union,Optional
+from pydantic import BaseModel
+from router import menu,seasoning
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+app = FastAPI()
+app.include_router(menu.router)
+app.include_router(seasoning.router)
+@app.get("/")
+def root():
+    return {"msg":"welcome to root page"}
