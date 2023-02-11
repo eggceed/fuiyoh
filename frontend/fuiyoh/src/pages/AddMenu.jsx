f7cdc3787b79dca6873e7ce2af2b58a07d23cf80
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2023-02-12 03:41:26 +0700

    Add decoration for Add menu page

--- frontend/fuiyoh/src/pages/AddMenu.jsx
+++ frontend/fuiyoh/src/pages/AddMenu.jsx
@@ -3,6 +3,7 @@ import '../styles/AddMenu.css'
 import menu from '../assets/menu.png'
 import axios from 'axios'
 import { useNavigate } from "react-router-dom"
+import Button from '../components/Button'
 
 
 const AddMenu = () => {
@@ -32,59 +33,60 @@ const AddMenu = () => {
     return (
     <Fragment>
       <div className="add-menu">
-          <h1>เพิ่มเมนู.</h1>
+          <h1>เพิ่มเมนู</h1>
       </div>
 
         <form method="post" onSubmit={handleSubmit}>
-        <div className="top">
+        <div className="all">
 
+            <div className="left">
             <div className="add-first-info">
-                <li>
-                    ชื่อ:
+                <div className="name-img"> 
+                <b>เมนู:</b>
+                    <li>
+                        ชื่อ:
+                    </li>
                     <input className="menu-name" name="menu_name"/>
-                </li>
-                <li>
-                    รูป:
+                    <li>
+                        รูป (ใส่ url):
+                    </li>
                     <textarea className="menu-img" name="menu_url" onChange={(e) => {setImg(e.target.value)}}/>
-                </li>
-                วัตถุดิบ:
-                <li>
-                    ชื่อ (คั่นวัตถุดิบด้วย ,):
-                </li>
-                <textarea className="ingrediants" name="ingredient"/>
-            </div>
-        
-            <div className="img-section">
-                <img className="default-menu-image" src={imgstate || menu}/>
+                </div>
+                <div className="ingredients-section">
+                    <b>วัตถุดิบ:</b>
+                    <li>
+                        ชื่อวัตถุดิบ (คั่นด้วย <b>,</b>):
+                    </li>
+                    <textarea className="ingrediants" name="ingredient"/>
+                </div>
+
+            
             </div>
 
             <div className="seasoning">
-                เครื่องปรุง:
-                <div className="seasoning-name">
-                    ชื่อ  
-                    <li>
-                        <input className="seasoning-input" value="เกลือ"/>
-                    </li>
-                    <li>
-                        <input className="seasoning-input" value="MSG"/>
-                    </li>
+             <div className="seasoning-name">     
+                <b>เครื่องปรุง:</b>
+                <li>
+                    <label className="salt">เกลือ <input className="seasoning-input" name="salt_gram"/> g </label>
+                </li>
+                <li>
+                    <label className="msg">MSG <input className="seasoning-input" name="msg_gram"/> g </label>
+                </li>
+            </div>
 
-                </div>
+                <div className="seasoning-amount">      
+                </div>        
+            </div>
+            </div>
+            <div className="right">  
+                <img className="default-menu-image" src={imgstate || menu}/>
+            </div>
 
-                <div className="seasoning-amount">
-                    ปริมาตร
-                    <li>
-                        <input className="seasoning-input" name="salt_gram"/> g
-                    </li>
-                    <li>
-                        <input className="seasoning-input" name="msg_gram"/> g
-                    </li>
-                </div>
+            </div>
+            <div className="add-button">
+                <Button name="Add Menu" > Add Menu </Button>
             </div>
             
-        </div>
-
-        <button name="add menu"> Add menu </button>
         </form>
 
         
